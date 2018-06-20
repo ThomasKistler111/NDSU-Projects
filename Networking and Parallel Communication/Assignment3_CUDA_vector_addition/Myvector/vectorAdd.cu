d60b09a8952e0e51bddaba8fa959e13de5d73b99
ThomasKistler111 <36827590+ThomasKistler111@users.noreply.github.com>
2018-06-20 16:37:40 -0500

    Update vectorAdd.cu

--- Networking and Parallel Communication/Assignment3_CUDA_vector_addition/Myvector/vectorAdd.cu
+++ Networking and Parallel Communication/Assignment3_CUDA_vector_addition/Myvector/vectorAdd.cu
@@ -1,21 +1,4 @@
-/**
- * Copyright 1993-2014 NVIDIA Corporation.  All rights reserved.
- *
- * Please refer to the NVIDIA end user license agreement (EULA) associated
- * with this source code for terms and conditions that govern your use of
- * this software. Any use, reproduction, disclosure, or distribution of
- * this software and related documentation outside the terms of the EULA
- * is strictly prohibited.
- *
- */
 
-/**
- * Vector addition: C = A + B.
- *
- * This sample is a very basic sample that implements element by element
- * vector addition. It is the same as the sample illustrating Chapter 2
- * of the programming guide with some additions like error checking.
- */
 
 #include <stdio.h>
 
